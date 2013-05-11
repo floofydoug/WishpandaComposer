502e5f2140a34494df6e0ef242cec0cbbe163602
Doug Phung <Doug@chillportal.com>
2013-05-11 01:58:00 -0400

    omniauth sign-in works for the most part

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ WishpandaComposer::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   resources :users
+
+  match "/auth/twitter/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130510205340) do
+ActiveRecord::Schema.define(:version => 20130511024230) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(:version => 20130510205340) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
