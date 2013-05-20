031eba0a5be7dea2de9cb7a2bf48d478ed1aec80
Alex Thomas <alex@chillportal.com>
2013-05-20 16:21:02 -0400

    Finished views. Creating an index for all contests.

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -2,4 +2,22 @@ class ContestsController < ApplicationController
   def new
     @contest = Contest.new
   end
+
+  def create
+    @contest = Contest.new(params[:contest])
+    if @contest.save
+    	
+      # Composer is making this red for some reason
+      # We want it to be green
+      flash[:success] = "Contest created!"
+      redirect_to contest_path(@contest)
+    else
+   	  flash[:error] = "Contest not created!"
+   	  redirect_to new_contest_path
+    end
+  end
+
+  def show
+    @contest = Contest.find(params[:id])
+  end
 end
--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,5 +2,18 @@ class Contest < ActiveRecord::Base
   attr_accessible :description, :end_date, :name, :start_date
   attr_accessible :image
 
+  # Image
+  # The hashtag at the end of the sizes maintains proportions
+  has_attached_file :image,
+                    :styles => { :normal => "100x100#", :thumbnail => "30x30#" },
+                    :url  => "/assets/contests/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/contests/:id/:style/:basename.:extension"
   
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
