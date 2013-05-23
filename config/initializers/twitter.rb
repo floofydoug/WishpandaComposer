f367aeb714a042074546d428c1c5412625a88685
Doug Phung <Doug@chillportal.com>
2013-05-23 17:53:34 -0400

    added config/initializer/twitter.rb

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "YX4uQcPXYbqhY0CTEee1cA"
+  config.consumer_secret = "mDF4D1ui0OSIXP1dcT22YZRvD2lgDTAgqqN7jSBrk"
+  config.oauth_token = "216810199-xVkrhdy7WHynLO6ETbJjaJMPYAj58OyUbu3MklIg"
+  config.oauth_token_secret = "1YHq62qtqdLTvBUF686gAPhjZHmawVRiE4Hdk9JV0"
+end
